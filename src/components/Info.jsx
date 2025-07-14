c4ce28790bbc03146c45ee3f0fbb9d297c337298
Kassim Amir <amir.kassim946@gmail.com>
2025-07-14 22:50:37 +0300

    commit initial files of react app, digital business card

---
+++ src/components/Info.jsx
@@ -0,0 +1,15 @@
+function Info() {
+    return (
+        <>
+            <img src="./assests/" alt="this is amir's photo"/>"
+            <div>
+                 <h2>Kassim Amir</h2>
+                <p>Frontend Developer</p>
+                <p>Email: amir.kassim946@gmail.com</p>
+                <p>LinkedIn: your-linkedin-profile</p>
+            </div>
+        </>
+    )
+}
+
+export default Info
