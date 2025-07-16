f279c6a765c1283bb692020b22b18f03c5c5ce79
Kassim Amir <amir.kassim946@gmail.com>
2025-07-16 22:04:45 +0300

    update these files

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Info  from './components/Info'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
       <Info />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+function About () {
+    return (
+        <div className="about-container">
+            <h2>About Me</h2>
+            <p>I am a frontend developer with a passion for creating beautiful and functional user interfaces.</p>
+        </div>
+    )
+}
+
+export default About
